26ca725e159eb4c136e5a4c9d7cbc0757fff61fe
Jay Nowak <135918510+JayBoyAdvance@users.noreply.github.com>
2023-12-16 01:54:35 -0500

    Removed unnecessary shell files and implemented some into the Midori OS build system

--- build-kodi-gbm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Builds Kodi for GBM
-
-num_proc=`nproc`
-echo "Using $num_proc processors"
-mkdir -p /home/kodi/kodi-build
-cd /home/kodi/kodi-build
-cmake --build . -- -j$num_proc
--- build-kodi-x11.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Builds Kodi for X11.
-
-num_proc=`nproc`
-echo "Using $num_proc processors"
-mkdir -p /home/kodi/kodi-build
-cd /home/kodi/kodi-build
-cmake --build . -- -j$num_proc
